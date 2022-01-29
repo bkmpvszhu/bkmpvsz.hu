64e6bb1f0ff6f9908b76727ffacd1436e3d2c258
Zoltan <meszaroszoltan@outlook.com>
2022-01-29 15:09:34 +0100

    Initial commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { siteAuthor } from '$lib/config'
+</script>
+
+<footer>
+
+	<nav>
+		Elérhetőségeink:
+		<ul>
+			<li>
+				<span class="material-icons-outlined">location_on</span>
+				6000 Kecskemét, Deák Ferenc tér 3
+			</li>
+			<li>
+				<span class="material-icons-outlined">call</span>
+				+36/20-952-2306
+			</li>
+			<li>
+				<span class="material-icons-outlined">fax</span>
+				+36/76-952-2306
+			</li>
+			<li>
+				<span class="material-icons-outlined">email</span>
+				kozeplok@vipmail.com
+			</li>
+			<li>
+				<span class="material-icons-outlined">e8a6</span>
+				Mészáros Zoltán elnök
+			</li>
+		</ul>
+	</nav>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/api/rss.xml" rel="external">RSS</a>
+			</li>
+			<li>
+				<a href="/">Home</a>
+			</li>
+		</ul>
+	</nav>
+
+	<p>&copy;{new Date().getFullYear()} {siteAuthor}</p>
+</footer>
---
+++ src/lib/config.js
@@ -0,0 +1,34 @@
+export const siteTitle = 'Bács-Kiskun Megyei Polgári Védelmi Szövetség'
+export const siteDescription = 'A Bács-Kiskun Megyei Polgári Védelmi Szövetség weboldala'
+export const siteURL = 'example.com' //TODO
+export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter' //TODO
+export const siteAuthor = 'Bács-Kiskun Megyei Polgári Védelmi Szövetség'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 15
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = [
+	{
+		title: 'Hírek',
+		route: '/blog/category/hirek'
+	}, {
+		title: 'Ifjusági események',
+		route: '/blog/category/ifjusag'
+	}, {
+		title: 'Felkészítések, képzések',
+		route: '/blog/category/kepzesek'
+	},
+	{
+		title: 'Támogatóink',
+		route: '/tamogatoink'
+	},
+	{
+		title: 'Számviteli beszámolók',
+		route: '/szamviteli-beszamolok'
+	},
+	{
+		title: 'Benke László Emlékplakett',
+		route: '/emlekplakett'
+	},
+]
