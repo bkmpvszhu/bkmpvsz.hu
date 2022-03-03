9a9647b4af29bb41bb1dc7c4d257e1c2ef262513
meszaroszoltan <36012223+meszaroszoltan@users.noreply.github.com>
2022-03-03 09:39:39 +0100

    beszamlolok

--- src/routes/szamviteli-beszamolok.svelte
+++ src/routes/szamviteli-beszamolok.svelte
@@ -2,38 +2,25 @@
 
 <h1>Számviteli beszámolók és közhasznúsági mellékletek</h1>
 
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41707.pdf">2011. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41708.pdf">2012. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41709.pdf">2013. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41710.pdf">2014. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41711.pdf">2015. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41712.pdf">2016. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
-<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41713.pdf">2017. évi számviteli beszámoló és közhasznúsági
-	mellékletei</a></p>
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76133.pdf">2020. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
 
-<h2>Kapcsolódó dokumentumok:</h2>
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76132.pdff">2019. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
 
-<p>
-	<a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76133.pdf" target="_blank">
-		<i class="far fa-file"></i>
-		2020. évi számviteli beszámoló és közhasznúsági mellékletei (2021.08.31.) </a>
-</p>
-<p>
-	<a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76132.pdf" target="_blank">
-		<i class="far fa-file"></i>
-		2019. évi számviteli beszámoló és közhasznúsági mellékletei (2021.08.31.) </a>
-</p>
-<p>
-	<a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76131.pdf" target="_blank">
-		<i class="far fa-file"></i>
-		2018. évi számviteli beszámoló és közhasznúsági mellékletei (2021.08.31.) </a>
-</p>
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2021-08/76131.pdf">2018. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41713.pdf">2017. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41712.pdf">2016. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41711.pdf">2015. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41710.pdf">2014. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41709.pdf">2013. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41708.pdf">2012. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
+
+<p><a href="https://bacs.katasztrofavedelem.hu/application/uploads/documents/2018-08/41707.pdf">2011. évi számviteli beszámoló és közhasznúsági mellékletei</a></p>
 
 <style>
 	h1 {
